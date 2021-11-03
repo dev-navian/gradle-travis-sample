bf172e3ea14abc55cdecb4455a8f51b9efe69176
dev-navian <ivan.annan@turntabl.io>
2021-11-03 18:28:29 +0000

    corporate client test added

---
+++ src/main/java/io/turntabl/ClientRegister.java
@@ -0,0 +1,12 @@
+package io.turntabl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientRegister {
+    // should have a list of both Corporate and Private Clients
+    List<CorporateClient> corporateClients = new ArrayList<>();
+    List<PrivateClient> privateClients = new ArrayList<>();
+
+
+}
---
+++ src/test/java/io/turntabl/CorporateClientTest.java
@@ -0,0 +1,32 @@
+package io.turntabl;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CorporateClientTest extends TestCase {
+
+    public void testGetClientServiceLevel() {
+        AccountManager accountManager = new AccountManager("Thomas Winans");
+        CorporateClient corporateClient = new CorporateClient("Thomas Winans", "4CDE2", ServiceLevel.Platinum, accountManager);
+
+        // getting client service level - should pass
+        assertEquals(ServiceLevel.Platinum, corporateClient.getServiceLevel());
+        // getting client service level - should fail
+        assertEquals(ServiceLevel.Gold, corporateClient.getServiceLevel());
+    }
+
+
+    public void testGetAccountManagerName() {
+
+        AccountManager accountManager = new AccountManager("James Riverson");
+        CorporateClient corporateClient = new CorporateClient("Peter Andrews", "ACS21", ServiceLevel.Gold, accountManager);
+
+        // getting the client name - should pass
+        assertEquals("James Riverson", corporateClient.getName());
+        // getting the client name - should fail
+        assertEquals("James Riversson", corporateClient.getName());
+
+
+    }
+}
